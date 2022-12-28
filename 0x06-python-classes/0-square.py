78a6052026ccfe9d84d346fabe981367564caff2
Crizy27 <crisssamuelserunjogi@gmail.com>
2022-12-28 09:23:20 +0000

    0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-import sys
 
 
-def safe_function(fct, *args):
-    try:
-        return fct(*args)
-    except Exception as err:
-        err = "Exception: " + str(err) + "\n"
-        sys.stderr.write(err)
-        return None
+class Square:
+    """An empty class Square."""
+    pass
+
+
+if __name__ == '__main__':
+    my_square = Square()
+    print(type(my_square))
+    print(my_square.__dict__)
