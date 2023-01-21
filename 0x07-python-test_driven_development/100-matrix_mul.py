d730d329c4fac9d339aa56f6bed430a50aa9c232
Crizy27 <crisssamuelserunjogi@gmail.com>
2023-01-21 20:16:15 +0000

    All tasks

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -1,118 +1,60 @@
 #!/usr/bin/python3
-
-
-class Matrix:
-    """Matrix.
-    Attributes:
-        name (str): name of matrix to use for error handling
-    """
-    name = None
-
-    @classmethod
-    def set_name(cls, name, matrix):
-        """set_name class method, alternative constructor
-        Alternative constructor that provides a way to customize error handling
-        messages to include name of matrix
-        Args:
-            name (str): name of matrix.
-            matrix (list): list of lists
-        """
-        cls.name = name
-        return cls(matrix)
-
-    def __init__(self, matrix):
-        """__init__ method.
-        Args:
-            matrix (list): list of lists
-        """
-        self.matrix = matrix
-
-    @property
-    def matrix(self):
-        """matrix: returns matrix object
-        Args:
-            matrix (list): list of lists
-        Returns:
-            matrix object
-        Raises:
-            TypeError: if not a list or list of lists, not ints and floats, not
-                uniform in row width
-            ValueError: if empty or cannot be multiplied
-        """
-        return self.__matrix
-
-    @matrix.setter
-    def matrix(self, matrix):
-        if not isinstance(matrix, list):
-            raise TypeError("{:s} must be a list".format(Matrix.name))
-        if len(matrix) == 0:
-            raise ValueError("{:s} can't be empty".format(Matrix.name))
-        rows = len(matrix)
-        columns = len(matrix[0])
-        if columns == 0:
-            raise ValueError("{:s} can't be empty".format(Matrix.name))
-        for r in range(rows):
-            if len(matrix[r]) != columns:
-                raise TypeError(
-                    "each row of {:s} must should be of the same size".format(
-                        Matrix.name))
-
-        for r in range(rows):
-            for c in range(columns):
-                n = matrix[r][c]
-                if not isinstance(n, int) and not isinstance(n, float):
-                    raise TypeError(
-                        "{:s} should contain only integers or floats".format(
-                            Matrix.name))
-
-        self.__matrix = matrix
-
-    def __len__(self):
-        """Provides length functionality to matrix object."""
-        return len(self.__matrix)
-
-    def __getitem__(self, index):
-        """Provides indexing functionality to matrix object."""
-        return self.__matrix[index]
+"""
+Matrix_mul module
+Function that multiplies two matrix
+"""
 
 
 def matrix_mul(m_a, m_b):
-    """matrix_mul multiplies two matrix objects
-    Note:
-        Most of the error handling is done in the Matrix class
+    """Return: a nex matrix with the product of m_a and m_b
     Args:
-    m_a (Matrix obj):
-    m_b (Matrix obj):
-    Returns:
-        matrix object that is the result of matrix multiplication
-    Raises:
-        ValueError: if matrices cannot be multiplied
+    param1: m_a type list of list is a matrix
+    param2: m_b type list of list is a matrix
+    Raise: TypeError, ValueError
     """
-    m_a = Matrix.set_name('m_a', m_a)
-    m_b = Matrix.set_name('m_b', m_b)
+    if not isinstance(m_a, list):
+        raise TypeError("m_a must be a list")
+    if not isinstance(m_b, list):
+        raise TypeError("m_b must be a list")
+    if not all(isinstance(row, list) for row in m_a):
+        raise TypeError("m_a must be a list of lists")
+    if not all(isinstance(row, list) for row in m_b):
+        raise TypeError("m_b must be a list of lists")
+    if m_a == [] or m_a == [[]]:
+        raise ValueError("m_a can't be empty")
+    if m_b == [] or m_b == [[]]:
+        raise ValueError("m_b can't be empty")
 
-    rows_a = len(m_a)
-    columns_a = len(m_a[0])
-    rows_b = len(m_b)
-    columns_b = len(m_b[0])
+    if not all((isinstance(element, (int, float)))
+               for element in [number for row in m_a for number in row]):
+        raise TypeError("m_a should contain only integers or floats")
+    if not all((isinstance(element, (int, float)))
+               for element in [number for row in m_b for number in row]):
+        raise TypeError("m_b should contain only integers or floats")
 
-    if columns_a != rows_b:
+    if not all(len(row) == len(m_a[0]) for row in m_a):
+        raise TypeError("each row of m_a must be of the same size")
+    if not all(len(row) == len(m_b[0]) for row in m_b):
+        raise TypeError("each row of m_b must be of the same size")
+
+    if len(m_a[0]) != len(m_b):
         raise ValueError("m_a and m_b can't be multiplied")
 
-    row = []
-    matrix = []
-    for i in range(rows_a):
-        for j in range(columns_b):
-            sums = 0
-            for k in range(rows_b):
-                sums += m_a[i][k] * m_b[k][j]
-            row.append(sums)
-        matrix.append(row)
-        row = []
-    return matrix
+    reverse_matrix = []
+    for i in range(len(m_b[0])):
+        new_row = []
+        for j in range(len(m_b)):
+            new_row.append(m_b[j][i])
+        reverse_matrix.append(new_row)
 
+    new_matrix = []
+    for irow in m_a:
+        new_row = []
+        for jcol in reverse_matrix:
+            value = 0
+            for k in range(len(reverse_matrix[0])):
+                value += irow[k] * jcol[k]
+            new_row.append(value)
+        new_matrix.append(new_row)
 
-if __name__ == '__main__':
-    print(matrix_mul([[1, 2], [3, 4]], [[1, 2], [3, 4]]))
-    print(matrix_mul([[1, 2]], [[3, 4], [5, 6]]))
-
+    return (new_matrix)
--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,26 +1,21 @@
 #!/usr/bin/python3
-
-""" say_my_name
-Prints My name is `first_name` `last_name`
-If either argument is not a string it raises TypeError
+"""
+sy_my_name module
+This function prints first and last name
 """
 
 
 def say_my_name(first_name, last_name=""):
-    """ add_integer - prints first and last name
+    """Return: the first and last name
+    Args:
+    param1: type str first name
+    param2: type str last name
+    Raise: TypeError
     """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-    print('My name is {:s} {:s}'.format(first_name, last_name))
 
+    print("My name is {} {}".format(first_name, last_name))
 
-if __name__ == '__main__':
-    say_my_name("John", "Smith")
-    say_my_name("Walter", "White")
-    say_my_name("Bob")
-    try:
-        say_my_name("Bob", 12)
-    except Exception as e:
-        print(e)
