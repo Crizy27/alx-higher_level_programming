199163e9f255e617a915153f6c6944c363e318f1
Crizy27 <crisssamuelserunjogi@gmail.com>
2023-02-05 19:23:07 +0000

    1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,29 +1,13 @@
 #!/usr/bin/python3
+"""MyList module.
+Contains a class MyList that inherits from list
+and a method that prints the sorted list.
+"""
 
 
 class MyList(list):
-    def print_sorted(self):
-        """Use bubble sort to create a sorted list to print
-        Args:
-            self (list): unsorted list
-        """
-        arr = self[:]
-        done = False
-        while not done:
-            done = True
-            for i in range(len(arr)-1):
-                if arr[i] > arr[i+1]:
-                    arr[i], arr[i+1] = arr[i+1], arr[i]
-                    done = False
-        print(arr)
+    """Defines the MyList class."""
 
-if __name__ == '__main__':
-    my_list = MyList()
-    my_list.append(1)
-    my_list.append(4)
-    my_list.append(2)
-    my_list.append(3)
-    my_list.append(5)
-    print(my_list)
-    my_list.print_sorted()
-    print(my_list)
+    def print_sorted(self):
+        """Prints the sorted list."""
+        print(sorted(self))
