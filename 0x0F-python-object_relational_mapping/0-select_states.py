7b7abcd8dcce67bf7cbebb4f3bb70209d2155385
Crizy27 <crisssamuelserunjogi@gmail.com>
2023-03-17 19:41:14 +0000

    0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python3
-"""
-Script that lists all `states` from the database `hbtn_0e_0_usa`.
-Arguments:
-    mysql username (str)
-    mysql password (str)
-    database name (str)
+"""Script displays states in database by ascending id order
+   Script should take 3 arguments:
+   mysql username, mysql password and database name
 """
 
-import sys
-import MySQLdb
+if __name__ == '__main__':
+    import sys
+    import MySQLdb
 
-if __name__ == "__main__":
-    mySQL_u = sys.argv[1]
-    mySQL_p = sys.argv[2]
-    db_name = sys.argv[3]
-
-    # By default, it will connect to localhost:3306
-    db = MySQLdb.connect(user=mySQL_u, passwd=mySQL_p, db=db_name)
-    cur = db.cursor()
-
-    cur.execute("SELECT * FROM states ORDER BY id")
-    rows = cur.fetchall()
-
-    for row in rows:
-        print(row)
+    if len(sys.argv) > 3:
+        conn = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            charset="utf8"
+        )
+        cur = conn.cursor()
+        cur.execute("""SELECT * FROM states ORDER BY states.id ASC""")
+        query_rows = cur.fetchall()
+        for row in query_rows:
+            print(row)
+        cur.close()
+        conn.close()
